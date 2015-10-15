155d43fdc41ebfe6907440ee807956537622b7c9
Stephan Hoyer <shoyer@climate.com>
2015-10-15 10:15:21 -0700

    change version identifier

--- setup.py
+++ setup.py
@@ -72,42 +72,42 @@ write_version = True
 
 if not ISRELEASED:
     import subprocess
-    FULLVERSION += '.dev'
+    FULLVERSION += '+12345.g43ab582'
 
-    pipe = None
-    for cmd in ['git', 'git.cmd']:
-        try:
-            pipe = subprocess.Popen(
-                [cmd, "describe", "--always", "--match", "v[0-9]*"],
-                stdout=subprocess.PIPE)
-            (so, serr) = pipe.communicate()
-            if pipe.returncode == 0:
-                break
-        except:
-            pass
+    # pipe = None
+    # for cmd in ['git', 'git.cmd']:
+    #     try:
+    #         pipe = subprocess.Popen(
+    #             [cmd, "describe", "--always", "--match", "v[0-9]*"],
+    #             stdout=subprocess.PIPE)
+    #         (so, serr) = pipe.communicate()
+    #         if pipe.returncode == 0:
+    #             break
+    #     except:
+    #         pass
 
-    if pipe is None or pipe.returncode != 0:
-        # no git, or not in git dir
-        if os.path.exists('xray/version.py'):
-            warnings.warn("WARNING: Couldn't get git revision, using existing xray/version.py")
-            write_version = False
-        else:
-            warnings.warn("WARNING: Couldn't get git revision, using generic version string")
-    else:
-        # have git, in git dir, but may have used a shallow clone (travis does this)
-        rev = so.strip()
-        # makes distutils blow up on Python 2.7
-        if sys.version_info[0] >= 3:
-            rev = rev.decode('ascii')
+    # if pipe is None or pipe.returncode != 0:
+    #     # no git, or not in git dir
+    #     if os.path.exists('xray/version.py'):
+    #         warnings.warn("WARNING: Couldn't get git revision, using existing xray/version.py")
+    #         write_version = False
+    #     else:
+    #         warnings.warn("WARNING: Couldn't get git revision, using generic version string")
+    # else:
+    #     # have git, in git dir, but may have used a shallow clone (travis does this)
+    #     rev = so.strip()
+    #     # makes distutils blow up on Python 2.7
+    #     if sys.version_info[0] >= 3:
+    #         rev = rev.decode('ascii')
 
-        if not rev.startswith('v') and re.match("[a-zA-Z0-9]{7,9}", rev):
-            # partial clone, manually construct version string
-            # this is the format before we started using git-describe
-            # to get an ordering on dev version strings.
-            rev = "v%s.dev-%s" % (VERSION, rev)
+    #     if not rev.startswith('v') and re.match("[a-zA-Z0-9]{7,9}", rev):
+    #         # partial clone, manually construct version string
+    #         # this is the format before we started using git-describe
+    #         # to get an ordering on dev version strings.
+    #         rev = "v%s.dev-%s" % (VERSION, rev)
 
-        # Strip leading v from tags format "vx.y.z" to get th version string
-        FULLVERSION = rev.lstrip('v')
+    #     # Strip leading v from tags format "vx.y.z" to get th version string
+    #     FULLVERSION = rev.lstrip('v')
 
 else:
     FULLVERSION += QUALIFIER
